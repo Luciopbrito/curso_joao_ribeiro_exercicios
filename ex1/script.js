0cc2b8ffd1ec21839bb2329cb9613ce2c05a267f
Lúcio Brito <luciopereirabritoo@gmail.com>
2022-02-19 00:36:58 -0300

    melhorado script ex1

--- ex1/script.js
+++ ex1/script.js
@@ -6,36 +6,27 @@ const btn = document.getElementsByTagName('button')[0];
 const res = document.querySelector('.res');
 
 btn.addEventListener('click', () => {
-    if (txt1.value != '' || txt2.value != '') {
+    if (txt1.value == '' || txt2.value == '') {
+        alert('[ERRO] Valor inválido, digite um valor');
+        res.innerHTML = ''
+    } else {
         let value1 = Number(txt1.value);
         let value2 = Number(txt2.value);
         res.innerHTML = '';
         console.clear();
-        soma(value1,value2);
-        subtracao(value1,value2);
-        divisao(value1,value2);
-        multiplicacao(value1,value2);
+
+        //somando
+        let soma = (n1,n2) => `<p>A soma entre ${n1} e ${n2} é igual a ${n1 + n2}</p>`;
+        //subtraindo
+        let subtracao = (n1,n2) => `<p>A subtração entre ${n1} e ${n2} é igual a ${n1 - n2}</p>`;
+        //dividindo
+        let divisao = (n1,n2) => `<p>A divisão entre ${n1} e ${n2} é igual a ${n1 / n2}</p>`;
+        //multiplicando
+        let multiplicacao = (n1,n2) => `<p>A multiplicação entre ${n1} e ${n2} é igual a ${n1 * n2}</p>`;
         
-        function soma(n1,n2) {
-            res.innerHTML += `<p>A soma entre ${n1} e ${n2} é igual a ${n1 + n2}</p>`;
-            console.log(`A soma entre ${n1} e ${n2} é igual a ${n1 + n2}`);
-        }   
-
-        function subtracao(n1,n2) {
-            res.innerHTML += `<p>A subtração entre ${n1} e ${n2} é igual a ${n1 - n2}</p>`;
-            console.log(`A subtração entre ${value1} e ${n2} é igual a ${n1 - n2}`);
-        }   
-
-        function divisao(n1,n2) {
-            res.innerHTML += `<p>A divisão entre ${n1} e ${n2} é igual a ${n1 / n2}</p>`;
-            console.log(`A divisão entre ${n1} e ${n2} é igual a ${n1 / n2}`)
-        }   
-
-        function multiplicacao(n1,n2) {
-            res.innerHTML += `<p>A multiplicação entre ${n1} e ${n2} é igual a ${n1 * n2}</p>`;
-            console.log(`A multiplicação entre ${n1} e ${n2} é igual a ${n1 * n2}`)
-        }
-    } else {
-        alert('[ERRO] Valor inválido, digite um valor');
+        res.innerHTML += soma(value1,value2);
+        res.innerHTML += subtracao(value1,value2);
+        res.innerHTML += divisao(value1,value2);
+        res.innerHTML += multiplicacao(value1,value2);
     }
 })
